c56c5193e98cd92eaf67281b14d2dc232dc759d2
ghani536 <ghani536@gmail.com>
2026-04-03 16:41:29 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@
  * PT. BISATANI - API Engine Pro (Universal Bridge)
  * Solusi Anti-CORS & Anti-Timeout untuk Semua Menu
  */
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycby-YMMm-34fJ1O4pzy91TH8_jE6evf5cfVD1aSoKzE_mH4HTqgk4ZFeqHe8Me8zEope/exec';
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxJrQHDeftJE6ZdvcA7tSFIfICzSphM4nqPBuA0ce9GFE4aOsOnAASboLbcPSR89t-E/exec';
 
 const api = {
     // 1. FUNGSI POST (KHUSUS SIMPAN: Absen Foto, Gaji, Karyawan)
